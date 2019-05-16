c21a5fef6d7d54d4647f64cd06f0330c8335501d
Josiah Turnquist <35823548+jturnquist276@users.noreply.github.com>
2019-05-15 19:07:25 -0700

    Create _CheckMovement.asm

---
+++ _CheckMovement.asm
@@ -0,0 +1,18 @@
+; (_CheckMovement.asm)
+
+include Pong.inc
+
+.code 
+CheckMovement proc,
+	duration: dword
+	; duration is the length of time to chill in milliseconds
+    pushad
+
+	
+	
+	
+
+    popad
+	ret
+CheckMovement endp
+end
