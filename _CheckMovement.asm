3a0fbebe759c23a5188294a36952a3b12b1e9ff9
Josiah Turnquist <35823548+jturnquist276@users.noreply.github.com>
2019-05-15 19:19:26 -0700

    Fixed parameters (probably)

--- _CheckMovement.asm
+++ _CheckMovement.asm
@@ -4,14 +4,18 @@ include Pong.inc
 
 .code 
 CheckMovement proc,
-	duration: dword
+	p1coord: PTR dword,
+	p2coord: PTR dword,
+	roomUpperBorder: PTR dword,
+	roomLowerBorder: PTR dword
 	; duration is the length of time to chill in milliseconds
     pushad
-
+	
+	call ReadKey
+	cmp al, Player1MoveUp
 	
 	
 	
-
     popad
 	ret
 CheckMovement endp
