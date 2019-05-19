283911eb17369338c27857a47e75cbc12e968bd0
Josiah Turnquist <35823548+jturnquist276@users.noreply.github.com>
2019-05-18 18:17:45 -0700

    fixed CheckMovement proc.

--- _CheckMovement.asm
+++ _CheckMovement.asm
@@ -6,7 +6,7 @@ include Pong.inc
 CheckMovement proc,
 	p1coord: PTR dword,						; y-coord
 	p2coord: PTR dword,						; y-coord
-	controls: ADDR byte,					; array for controls
+	controls: PTR byte,					; array for controls
 	paddleHeight: PTR dword,
 	roomUpperBorder: PTR dword,
 	roomLowerBorder: PTR dword
@@ -20,15 +20,15 @@ CheckUpKeyP1:
 	jae CheckDownKeyP1
 	; check for button-pressed -- skip if player1 is above or equal to room's upper border
 	call ReadKey
-	cmp al, [controls]		; first element of controls' array
+	cmp al, byte PTR [controls]		; first element of controls' array
 CheckDownKeyP1:
 	mov eax, p1coord
 	sub eax, 0								; collision-checking is with bottom of paddle
 	cmp eax, roomLowerBorder
-	jbe 
+	jbe CheckUpKeyP2
 	; check for button-pressed -- skip if player1 is below or equal to room's lower border
 	call ReadKey
-	cmp al, [controls + 1]	; second element of controls' array
+	cmp al, byte PTR [controls + 1]	; second element of controls' array
 CheckUpKeyP2:
 	mov eax, p2coord
 	sub eax, paddleHeight					; check with top of paddle rather than bottom.
@@ -36,16 +36,16 @@ CheckUpKeyP2:
 	jae CheckDownKeyP2
 	; check for button-pressed -- skip if player1 is above or equal to room's upper border
 	call ReadKey
-	cmp al, [controls + 3]		; third element of controls' array
+	cmp al, byte PTR [controls + 3]		; third element of controls' array
 CheckDownKeyP2:
 	mov eax, p1coord
 	sub eax, 0								; collision-checking is with bottom of paddle
 	cmp eax, roomLowerBorder
-	jbe 
+	jbe endit
 	; check for button-pressed -- skip if player1 is below or equal to room's lower border
 	call ReadKey
-	cmp al, [controls + 4]	; fourth element of controls' array	
-    
+	cmp al, byte PTR [controls + 4]	; fourth element of controls' array	
+endit:
 	popad
 	
 	ret
